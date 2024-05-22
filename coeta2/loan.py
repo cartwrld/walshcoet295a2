d5341e7ede8cc6101dd80032cc79a9e8aa87e6b8
Carter Walsh <cartwalsh@gmail.com>
2024-05-22 13:27:00 -0600

    hehehehe

--- coeta2/loan.py
+++ coeta2/loan.py
@@ -30,8 +30,6 @@ df = pd.read_csv("data/loan.csv")
 
 df.drop(['occupation'], axis=1, inplace=True)
 
-
-
 print(df.head().to_string())
 
 print("\n\n Shape and Size")
@@ -40,6 +38,52 @@ print(df.shape)
 print(df.dtypes)
 
 print(type(df))
-print(df[0])
+# print(df[0])
 
-df_gender = df[df['gender'] == 'Male']
+# df_copy = df
+#
+# df['gen'] = np.NAN
+
+for i, j in enumerate(df['gender']):
+    if j == 'Male':
+        df.loc[i:i, 'gender'] = 0
+    else:
+        df.loc[i:i, 'gender'] = 1
+
+for i, j in enumerate(df['education_level']):
+    if j == 'High School':
+        df.loc[i:i, 'education_level'] = 0
+    elif j == "Bachelor's":
+        df.loc[i:i, 'education_level'] = 1
+    else:
+        df.loc[i:i, 'education_level'] = 2
+
+for i, j in enumerate(df['marital_status']):
+    if j == 'Single':
+        df.loc[i:i, 'marital_status'] = 0
+    elif j == 'Married':
+        df.loc[i:i, 'marital_status'] = 1
+
+for i, j in enumerate(df['loan_status']):
+    if j == 'Denied':
+        df.loc[i:i, 'loan_status'] = 0
+    elif j == 'Approved':
+        df.loc[i:i, 'loan_status'] = 1
+
+sb.heatmap(df.corr(), annot=True)
+plt.show()
+
+df['gender'] = df['gender'].map({0: "Male", 1: "Female"})
+df['loan_status'] = df['loan_status'].map({0: "Approved", 1: "Denied"})
+
+loan_approved = df['loan_status'] == 'Approved'
+loan_denied = df['loan_status'] == 'Denied'
+
+# compare age with health
+plt.hist(df[loan_approved]['age'], color='b', alpha=0.5, bins=15, label="Approved")
+plt.hist(df[loan_denied]['age'], color='r', alpha=0.5, bins=15, label="Denied")
+plt.legend()
+plt.title("Health Count -vs- Age")
+plt.show()
+
+print(df.head().to_string())
