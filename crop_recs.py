0077a6e8fdee5fccc7f27c172f8317e062c21877
Carter Walsh <cartwalsh@gmail.com>
2024-05-23 13:05:58 -0600

    almost done? idk

--- crop_recs.py
+++ crop_recs.py
@@ -10,50 +10,18 @@ from tensorflow.keras.regularizers import l2
 df = pd.read_csv("data/Crop_Recommendation.csv")
 
 crops_arr = np.unique(df['Crop'])
-print(crops_arr)
 
 # create dict for the map to convert all vals to int at once
-crop_to_int = {crop: i for i, crop in enumerate(crops_arr)}
+crop_dict = {crop: i for i, crop in enumerate(crops_arr)}
 
-df['Crop'] = df['Crop'].map(crop_to_int)
-
-print(df.head().to_string())
-
+df['Crop'] = df['Crop'].map(crop_dict)
 
 # plt.figure(figsize=(10, 8))
 # sb.heatmap(df.corr(), annot=True)
 # plt.show()
 
-print(df['Crop'].value_counts())
-
-
-# def compile_model(optimizer, loss, title):
-#     Y = df['Crop'].astype('float32')
-#     X = df.drop("Crop", axis=1).astype('float32')
-#
-#     print(X.shape)
-#
-#     model = keras.Sequential()
-#     # model.add(layers.Dense(256, activation="relu"))  # input layer
-#     model.add(layers.Dense(32, activation="selu"))  # hidden layer 1
-#     model.add(layers.Dense(16, activation="selu"))  # hidden layer 1
-#     model.add(layers.Dense(7, activation="selu"))  # hidden layer 1
-#     model.add(layers.Dense(4, activation="selu"))  # hidden layer 1
-#     # model.add(layers.Dense(8, activation="selu"))  # hidden layer 2
-#     model.add(layers.Dense(22, activation='softmax'))  # output layer
-#     # model.add(layers.Dense(1, activation='sigmoid'))  # output layer
-#
-#     model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
-#
-#
-#
-#     results = model.fit(X, Y, validation_split=0.2, batch_size=32, epochs=50)
-#     acc_chart(results, title)
-#     loss_chart(results, title)
 
 def compile_model(optimizer, loss, title):
-
-
     Y = df['Crop'].astype('float32')
     X = df.drop("Crop", axis=1).astype('float32')
 
@@ -68,40 +36,52 @@ def compile_model(optimizer, loss, title):
 
     model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
 
-    results = model.fit(X, Y, validation_split=0.33, batch_size=10, epochs=50)
+    results = model.fit(X, Y, validation_split=0.33, batch_size=10, epochs=10)
 
     acc_chart(results, title)
     loss_chart(results, title)
 
-    test_predictions = [
-        [90, 42, 43, 20.88, 82.00, 6.50, 202.93],
-        [85, 58, 41, 21.77, 80.32, 7.04, 226.66],
-        [60, 55, 44, 23.00, 82.32, 7.84, 263.96]
-    ]
+    test_predictions = np.array([
+        np.array([90, 42, 43, 20.88, 82.00, 6.50, 202.93]),
+        np.array([85, 58, 41, 21.77, 80.32, 7.04, 226.66]),
+        np.array([60, 55, 44, 23.00, 82.32, 7.84, 263.96])
+    ])
 
-    print(test_predictions)
-
-
-    x_pred = np.array([test_predictions[0]], dtype=np.float64)
-
-    y_pred = (model.predict(x_pred) > 0.5).astype(int)
-
-    print(y_pred[0])
+    get_top_crops(test_predictions)
+    # print(test_predictions)
     #
-    # x_health = np.array([[50, 1, 2, 129, 196, 0, 0, 163, 0, 0, 0, 0, 0]], dtype=np.float64)
-    # y_health = (model.predict(x_health) > 0.5).astype(int)
-    # print(y_health[0])
+    # x_pred = np.array([test_predictions[0]], dtype=np.float64)
+    #
+    # y_pred = (model.predict(x_pred) > 0.5).astype(int)
+    #
+    # print(y_pred[0])
 
-    i = 0
 
-    for sample in test_predictions:
-        x_pred = np.array([sample], dtype=np.float64)
 
-        y_pred = (model.predict(x_pred) > 0.5).astype(int)
-        print(f"Sample {i}: {y_pred} => Top 2 recommended crops: {y_pred}")
-        i += 1
+def get_top_crops(preds):
+    """This function will take in a numpy array and determine the 2 highest
+    values and print out the corresponding crop strings"""
+    print(f"probabilities = {preds}")
 
+    first_place = preds.max()
+    print(first_place)
+    # index = crop_dict[]
+    highest_index = np.where(preds == first_place)[0][0]
+    highest_string = crop_dict[highest_index]
+
+    preds[highest_index] = -1
+
+    second_highest = max(preds)
+    second_highest_index = np.where(preds == second_highest)[0][0]
+    second_highest_string = crop_dict[second_highest_index]
+
+    print(f"highest prob = {first_place}")
+    print(f"highest_index = {highest_index}")
+    print(f"highest_string = {highest_string}")
+
+    print(f"2nd highest prob = {second_highest}")
+    print(f"2nd highest_index = {second_highest_index}")
+    print(f"2nd highest_string = {second_highest_string}")
 
 
 compile_model("adam", "sparse_categorical_crossentropy", "Adam/BCE")
-
